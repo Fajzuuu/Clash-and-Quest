d65a726f85949901be457f8fe190777030f82937
nanda <najibfina02@gmail.com>
2026-05-07 06:08:03 +0700

    Menambahkan class Warrior dan Mage

--- src/main/Warrior.java
+++ src/main/Warrior.java
@@ -3,17 +3,5 @@ public class Warrior extends Character {
         // Memanggil constructor Character: name, hp, attack, defense
         super(name, 150, 45, 25);
     }
-    @Override
-    public void attack(Character target) {
-        // Kalkulasi spesifik Warrior
-        int damageDealt = this.getAttackPower() - target.getDefense();
-        
-        // Sesuai aturan: minimum damage adalah 1 (bahkan jika defense musuh tinggi)
-        if (damageDealt < 1) {
-            damageDealt = 1;
-        }
 
-        System.out.println(this.getName() + " mengayunkan senjata dengan kuat!");
-        target.takeDamage(damageDealt);
-    }
 }
